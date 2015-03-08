0c649aca9d0964998e4fddf88b55e318679ff8bd
entireweb <entireweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-08 10:09:01 +0000

    # cd into your local copy of the repository and # make sure it's up to date
    $ cd my-local-dir/
    my-local-dir/$ svn up
    > At revision 11326.
    
    # Good: all up to date.  If there had been changes in the
    # central repository, they would have been downloaded and
    # merged into your local copy.
    
    # Edit the file that needs changing.  I use nano.
    # No need for editor wars; use whatever you like.
    
    my-local-dir/$ nano trunk/my-plugin.php
    # ... edit ... edit ... make typo ... edit
    # ... fix typo ... edit ... all done.
    
    # You can check and see what's different between your
    # local copy and the central repository.
    # First we check the status of the local copy.
    
    my-local-dir/$ svn stat
    > M	trunk/iOli.php
    
    # This tells us that our local trunk/iOli.php
    # is different from the copy we downloaded from the
    # central repository ( "M" for "modified" ).
    
    # Let's see what exactly has changed in that file,
    # so we can check it over and make sure things look right.
    
    my-local-dir/$ svn diff
    > * What comes out is essentially the result of a
      * standard `diff -u` between your local copy and the
      * original copy you downloaded.
    
    # Looks good.  Let's check in those changes to the
    # central repository.
    
    my-local-dir/$ svn ci -m "fancy new feature: now you can foo *and* bar at the same time"
    > Sending	trunk/iOli.php
    > Transmitting file data .
    > Committed revision 11327.
    
    # All done!
    
    git-svn-id: https://plugins.svn.wordpress.org/ioli/trunk@1108006 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ iOli.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Plugin Name: iOli
+Plugin URI: https://ioli.ru
+Description: A plugin that replaces shared links with iOli short urls and gives you the option of placing a widget on your site that shows your popular or most recent iOlimarks, or the top results from a search of all currently popular iOli links.  Please visit the plug-in settings page to authorize your iOli account.  For the widget, please find 'iOli iOlimarks' under the Available Widgets area.
+Version: 1.0
+Author: iOli
+*/
+	include("iOli.class.php");
+
+	if(is_admin()) {
+		add_action('admin_menu', array('PUS_Short','admin_menu'));
+		add_action('admin_init', array('PUS_Short','admin_setting'));
+	 	add_action('admin_enqueue_scripts', array('PUS_Short','admin_css'));
+	 	add_action('admin_enqueue_scripts', array('PUS_Short','admin_js'));
+		register_activation_hook(__FILE__, array('PUS_Short','install'));	 	
+		register_deactivation_hook(__FILE__, array('PUS_Short','uninstall')); 		
+	}
+	add_action('widgets_init', array('PUS_Short','register_widget'));			 	
+	add_action('wp_enqueue_scripts', array('PUS_Short','js'));
+	add_action('wp_enqueue_scripts', array('PUS_Short','css'));
+	add_action( 'add_meta_boxes', array('PUS_Short','add_to_post'), 10, 2 );
+	PUS_Short::start(get_option('shortener_settings'));
