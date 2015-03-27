6391d14343e8491bbc6651dc1f12087394610338
entireweb <entireweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-27 21:12:42 +0000

    # Make sure to use `svn cp` instead of the regular `cp`.
    my-local-dir/$ svn cp trunk tags/1.2
    > A tags/1.2
    
    # Now, as always, check in the changes.
    
    my-local-dir/$ svn ci -m "tagging version 1.2"
    > Adding         tags/1.2
    > Adding         tags/1.2/iOli.php
    > Adding         tags/1.2/readme.txt
    > Committed revision 11328.
    
    # All done!
    
    git-svn-id: https://plugins.svn.wordpress.org/ioli/trunk@1122486 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ urlshortener.js
@@ -0,0 +1,13 @@
+/*
+ * ====================================================================================
+ * ----------------------------------------------------------------------------------
+ *  The WordPress plugin is built for the iOli Shortener Link that can bought from 
+ *  iOli. This will not work with any other URL shorteners and I will not 
+ *  implement it to other scripts.
+ *
+ *  Version 1.3
+ *  Copyright (c) 2015 iOli - https://ioli.ru/v1/
+ * ----------------------------------------------------------------------------------
+ * ====================================================================================
+ */ 
+var getLocation=function(e){var t=document.createElement("a");t.href=e;return t};(function(e){e.fn.extend({shorten:function(t){var n={url:null,key:null,internal:false};var r=e.extend(n,t);var i=0;if(r.url===null){console.log("Please set the url to the api of the url shortener script.");i=1}if(r.key===null){console.log("Please set your API key.");i=1}if(i==0){e(this).each(function(){var t=e(this);var n=getLocation(t.attr("href"));if(n.hostname!=location.hostname){e.getJSON(r.url+"api?callback=?",{api:r.key,url:t.attr("href")},function(e){if(e.error=="0"){t.attr("href",e.short)}else{console.log(e.msg)}})}})}}});e(document).ready(function(){e(document).on("submit","#PUS_main form#PUS_form",function(t){t.preventDefault();var n=e(this);var r=e(this).parent("#PUS_main");var i=e(".current-container");var s=n.find("#PUS_url");var o=e(this).attr("action");var u=e("#PUS_share_text").val();if(!s.val()){r.find("#PUS_message").hide().html('Please enter a valid URL (including http:&#47;&#47;)').fadeIn("slow");r.find("#PUS_message").addClass("PUS_error")}else{r.find("#PUS_loading").fadeIn();e.getJSON(o+"api?callback=?",{api:r.find("#PUS_token").val(),url:s.val(),custom:r.find("#PUS_custom_input").val()},function(e){var t="<a href='https://twitter.com/share?url="+encodeURIComponent(e.short)+"&text="+encodeURI(u).replace(/%20/g,"+")+"' target='_blank'>Tweet</a> <a href='https://www.facebook.com/sharer.php?u="+e.short+"' target='_blank'>Share</a>";r.find("#PUS_loading").fadeOut();if(e.error){r.find("#PUS_message").hide().html(e.msg).fadeIn("slow");r.find("#PUS_message").addClass("PUS_error")}else{r.find("#PUS_message").hide();r.find("#PUS_custom_container").html(t);s.val(e.short);s.select()}})}});var t=e(".shortener_widget #PUS_main").width();if(t<450)e(".shortener_widget #PUS_main").addClass("widget_fix")})})(jQuery)
