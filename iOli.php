b68cceebd2cb15cdd3e59f5d500ccf930d8c4182
entireweb <entireweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-17 23:19:58 +0000

    # Make sure to use `svn cp` instead of the regular `cp`.
    my-local-dir/$ svn cp trunk tags/1.2
    > A tags/1.2
    
    # Now, as always, check in the changes.
    
    my-local-dir/$ svn ci -m "tagging version 1.2"
    > Adding         tags/1.2
    > Adding         tags/1.2/iOli.php
    > Adding         tags/1.2/readme.txt
    > Committed revision 11328.
    
    # All done!
    
    git-svn-id: https://plugins.svn.wordpress.org/ioli/trunk@1137638 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ iOli.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Plugin Name: iOli
+Plugin URI: https://ioli.ru
+Description: A plugin that replaces shared links with iOli Seo® short urls and gives you the option of placing a widget on your site that shows your popular or most recent iOlimarks, or the top results from a search of all currently popular iOli links. Please visit the plug-in settings page to authorize your iOli Seo® account. For the widget, please find 'iOli iOlimarks' under the Available Widgets area.
+Version: 1.5
+Author: iOli Seo
+Author URI: https://ioli.ru/v1/
+Disclaimer: Use at your own risk. No warranty expressed or implied is provided.
+
+Copyright 2012-2015 The iOli Seo® Team (email: info@ioli.ru, web: https://ioli.ru)
+*/
+	include("iOli.class.php");
+
+	if(is_admin()) {
+		add_action('admin_menu', array('PUS_Short','admin_menu'));
+		add_action('admin_init', array('PUS_Short','admin_setting'));
+	 	add_action('admin_enqueue_scripts', array('PUS_Short','admin_css'));
+	 	add_action('admin_enqueue_scripts', array('PUS_Short','admin_js'));
+		register_activation_hook(__FILE__, array('PUS_Short','install'));	 	
+		register_deactivation_hook(__FILE__, array('PUS_Short','uninstall')); 		
+	}
+	add_action('widgets_init', array('PUS_Short','register_widget'));			 	
+	add_action('wp_enqueue_scripts', array('PUS_Short','js'));
+	add_action('wp_enqueue_scripts', array('PUS_Short','css'));
+	add_action( 'add_meta_boxes', array('PUS_Short','add_to_post'), 10, 2 );
+	PUS_Short::start(get_option('shortener_settings'));
