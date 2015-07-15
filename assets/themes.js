78b0e8fa98de41c32a55b1938cf3755ef4c8dfc9
entireweb <entireweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-15 23:28:51 +0000

    # Make sure to use `svn cp` instead of the regular `cp`.
    my-local-dir/$ svn cp trunk tags/1.2
    > A tags/1.2
    
    # Now, as always, check in the changes.
    
    my-local-dir/$ svn ci -m "tagging version 1.2"
    > Adding         tags/1.2
    > Adding         tags/1.2/iOli.php
    > Adding         tags/1.2/readme.txt
    > Committed revision 11328.
    
    # All done!
    
    git-svn-id: https://plugins.svn.wordpress.org/ioli/trunk@1199667 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/themes.js
@@ -0,0 +1,48 @@
+/*
+ * ====================================================================================
+ * ----------------------------------------------------------------------------------
+ *  The WordPress plugin is built for the iOli® Shortener Link that can bought from 
+ *  iOli®. This will not work with any other URL shorteners and I will not 
+ *  implement it to other scripts.
+ *
+ *  Version 1.6
+ *  Copyright (c) 2015 iOli® - https://ioli.ru/
+ * ----------------------------------------------------------------------------------
+ * ====================================================================================
+ */
+jQuery(document).ready(function(){
+  jQuery(document).on('change',".PUS_theme-trigger",function(){
+  	var c=jQuery(this).val();
+  	jQuery("#PUS_widget_title").text(jQuery('.PUS_theme-trigger :selected').text());
+    jQuery("#PUS_demo").find("#PUS_main").removeClass();
+    jQuery("#PUS_demo").find("#PUS_main").addClass(c+"-c");
+    if(c=="cc"){
+    	jQuery(".PUS_custom_message").fadeIn();
+    }else{
+    	jQuery("#PUS_custom_message").hide();
+    }
+    jQuery('body,html').animate({ scrollTop: 0 });    	
+  });     
+  jQuery("#quick_url_shorten_admin_button").click(function(e){
+    e.preventDefault();
+    var c=jQuery("#quick_url_shorten_admin_form");
+    var appurl=c.attr("data-url");
+    var url=c.find("#quick_url_shorten_admin_input");
+
+    jQuery.getJSON(appurl+"api?callback=?",
+      {
+        api: c.find("#quick_url_shorten_admin_api").val(),
+        url: url.val(),
+        custom: c.find("#quick_url_shorten_admin_custom").val()
+      },
+      function(html) {
+        if(html.error){
+          c.find('#quick_url_shorten_admin_message').hide().html('<div id="message" class="error">'+html.msg+'</div>').fadeIn('slow');               
+        }else{          
+          c.find('#quick_url_shorten_admin_message').hide();
+          url.val(html.short);  
+          url.select();                      
+        }     
+    });        
+  });
+});
