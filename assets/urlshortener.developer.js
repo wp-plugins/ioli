807b7f77b60bcd200d25487d82d51792fe07cb52
entireweb <entireweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-15 22:50:09 +0000

    # Make sure to use `svn cp` instead of the regular `cp`.
    my-local-dir/$ svn cp trunk tags/1.2
    > A tags/1.2
    
    # Now, as always, check in the changes.
    
    my-local-dir/$ svn ci -m "tagging version 1.2"
    > Adding         tags/1.2
    > Adding         tags/1.2/iOli.php
    > Adding         tags/1.2/readme.txt
    > Committed revision 11328.
    
    # All done!
    
    git-svn-id: https://plugins.svn.wordpress.org/ioli/trunk@1199626 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/urlshortener.developer.js
@@ -0,0 +1,102 @@
+/*
+ * ====================================================================================
+ * ----------------------------------------------------------------------------------
+ *  The WordPress plugin is built for the iOli® Shortener Link that can bought from 
+ *  iOli®. This will not work with any other URL shorteners and I will not 
+ *  implement it to other scripts.
+ *
+ *  Version 1.5
+ *  Copyright (c) 2015 iOli® - https://ioli.ru/
+ * ----------------------------------------------------------------------------------
+ * ====================================================================================
+ */
+var getLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+}; 
+(function($){
+    // jShorten Method
+    $.fn.extend({ 
+        shorten: function(settings) {
+            var defaults = {
+              url:null,
+              key:null,
+              internal: false
+            };
+            var s = $.extend(defaults, settings);
+            var error=0;
+
+            if(s.url===null){
+              console.log('Please set the url to the api of the url shortener script.');
+                error=1;
+            }
+            if(s.key===null){
+                console.log('Please set your API key.');
+                error=1;              
+            }
+            if(error==0){  
+                $(this).each(function(){
+                  var e=$(this);
+                  var l = getLocation(e.attr("href"));
+                  if(l.hostname!=location.hostname){
+                    $.getJSON(s.url+"api?callback=?",
+                      {
+                        api: s.key,
+                        url: e.attr("href")
+                      },
+                      function(r) {
+                       if(r.error=='0'){
+                          e.attr('href',r.short);
+                       }else{
+                          console.log(r.msg);
+                       }      
+                    });               
+                  }
+                });
+            }                          
+        }
+    });
+    $(document).ready(function(){
+      // Shorten Ajax
+      $(document).on('submit',"#PUS_main form#PUS_form",function(e) {
+        e.preventDefault();
+        var form=$(this);
+        var main=$(this).parent("#PUS_main");
+        var current=$(".current-container");
+        var url=form.find("#PUS_url");
+        var appurl=$(this).attr("action");
+        var share_text=$("#PUS_share_text").val();
+        
+
+        if(!url.val()){
+          main.find('#PUS_message').hide().html('Please enter a valid URL (including http:&#47;&#47;)').fadeIn('slow');
+          main.find('#PUS_message').addClass("PUS_error");
+        }else{
+           main.find('#PUS_loading').fadeIn();
+          $.getJSON(appurl+"api?callback=?",
+            {
+              api: main.find("#PUS_token").val(),
+              url: url.val(),
+              custom: main.find("#PUS_custom_input").val()
+            },
+            function(html) {
+              var share_html="<a href='https://twitter.com/share?url="+encodeURIComponent(html.short)+"&text="+encodeURI(share_text).replace(/%20/g,'+')+"' target='_blank'>Tweet</a> <a href='https://www.facebook.com/sharer.php?u="+html.short+"' target='_blank'>Share</a>";              
+              main.find('#PUS_loading').fadeOut();
+              if(html.error){
+                main.find('#PUS_message').hide().html(html.msg).fadeIn('slow');               
+                main.find('#PUS_message').addClass("PUS_error");
+              }else{          
+                main.find('#PUS_message').hide();
+                main.find("#PUS_custom_container").html(share_html);
+                url.val(html.short);  
+                url.select();                      
+              }     
+          });                     
+        }
+      });   
+      // Fix Widget size
+      var w=$(".shortener_widget #PUS_main").width();
+      if(w<450) $(".shortener_widget #PUS_main").addClass("widget_fix");
+    });
+})(jQuery);
